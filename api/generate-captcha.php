8719713dfdea1872c45f6423d618a2744b6a54b9
6AshtonStyle9 <6ashtonstyle9@gmail.com>
2024-10-27 00:11:56 +0800

    finish generate captcha

--- api/generate-captcha.php
+++ api/generate-captcha.php
@@ -7,5 +7,6 @@ $b=  imagecolorallocate($i, 250, 250, 250);
 $t = imagecolorallocate($i, 0, 0, 0);
 imagefill($i, 0, 0, $b);
 imagestring($i, 5, 32.5, 13, $n, $t);
-image($image, "../img/captcha-image.png");
+imagepng($i, "../img/captcha-image.png");
+imagedestroy($i);
 $_SESSION["cn"] = $n;
